d8c2f4a5798f6778a0312300e4f74df7a98ffa0d
ThwyIgo <thiagopachecorocha@hotmail.com>
2025-07-16 14:16:12 -0300

    genChains generates the whole chain

--- app/RainbowTable.hs
+++ app/RainbowTable.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedRecordDot #-}
 
-module RainbowTable (genTable, lookup, genRandStrImpl, RainbowTable(..)) where
+module RainbowTable (genTable, lookup, genRandStrImpl, RainbowTable (..)) where
 
 import Control.Monad
 import Control.Monad.State.Lazy
@@ -12,12 +12,15 @@ import Prelude hiding (lookup)
 
 data RainbowTable = RainbowTable
   { table :: HashMap B.ByteString String,
-    genChain :: String -> (B.ByteString, String)
+    genChain :: String -> [(B.ByteString, String)],
+    chainLength :: ChainLength,
+    hash :: String -> B.ByteString,
+    reduce :: Integer -> B.ByteString -> String
   }
 
-type PwSize = Int
+type PwLength = Int
 
-type ChainSize = Int
+type ChainLength = Int
 
 type ChainCount = Int
 
@@ -36,25 +39,23 @@ genRandStrImpl charset randSeed charCount =
           (randomIndex, nextSeed) = randomR (0, charsetLength - 1) gen
        in (charset !! randomIndex, nextSeed)
 
--- Gera uma cadeia de aplicações de HashReduce. Retorna o último hash e a última senha
-genChainImpl :: (String -> B.ByteString) -> (Integer -> B.ByteString -> String) -> ChainSize -> String -> (B.ByteString, String)
+-- Gera uma cadeia de aplicações de HashReduce. Retorna pares de hash e senha na ordem da cadeia.
+genChainImpl :: (String -> B.ByteString) -> (Integer -> B.ByteString -> String) -> ChainLength -> String -> [(B.ByteString, String)]
 genChainImpl hash reduce chainSize initialPw =
-      let hashReduce i = do
-            currPw <- get
-            let hashed = hash currPw
-                reduced = reduce i hashed
-            put reduced
-
-          lastPw = flip execState initialPw $ sequence [hashReduce $ fromIntegral i | i <- [0 .. chainSize - 1]]
-          lastHash = hash lastPw
-       in (lastHash, lastPw)
+  let hashReduce i = do
+        currPw <- get
+        let hashed = hash currPw
+            reduced = reduce i hashed
+        put reduced
+        return (hashed, currPw)
+   in flip evalState initialPw $ sequence [hashReduce $ fromIntegral i | i <- [0 .. chainSize]]
 
 -- Gera uma rainbowtable (HashMap)
 genTableImpl ::
-  ([Char] -> StdGen -> PwSize -> (String, StdGen)) ->
+  ([Char] -> StdGen -> PwLength -> (String, StdGen)) ->
   ([String] -> [(B.ByteString, String)]) ->
   [Char] ->
-  PwSize ->
+  PwLength ->
   StdGen ->
   ChainCount ->
   (HashMap B.ByteString String, StdGen)
@@ -66,32 +67,38 @@ genTableImpl genRandStr genChains charset pwSize randSeed chainCount =
         return str
       (initialPws, nextSeed) = flip runState randSeed $ replicateM chainCount randStr'
       chains = genChains initialPws
-  in (HashMap.fromList chains, nextSeed)
+   in (HashMap.fromList chains, nextSeed)
 
 -- Gera uma rainbowtable imediatamente (Strict)
 genTable ::
-  ([Char] -> StdGen -> PwSize -> (String, StdGen)) ->
-  ([Char] -> B.ByteString) ->
-  ([Char] -> PwSize -> Integer -> B.ByteString -> String) ->
+  ([Char] -> StdGen -> PwLength -> (String, StdGen)) ->
+  (String -> B.ByteString) ->
+  ([Char] -> PwLength -> Integer -> B.ByteString -> String) ->
   [Char] ->
-  PwSize ->
-  ChainSize ->
+  PwLength ->
+  ChainLength ->
   ChainCount ->
   StdGen ->
   (RainbowTable, StdGen)
 genTable genRandStr hash reduce charset pwSize chainSize chainCount randSeed =
   let genChain = genChainImpl hash reduce' chainSize
       reduce' = reduce charset pwSize
-      genChains = map (\s -> s <$ genChain s)
+      genChains = map (\s -> s <$ last (genChain s))
       !(!rt, nextSeed) = genTableImpl genRandStr genChains charset pwSize randSeed chainCount
    in ( RainbowTable
           { table = rt,
-            genChain = genChain
+            genChain = genChain,
+            chainLength = chainSize,
+            hash = hash,
+            reduce = reduce'
           },
         nextSeed
       )
 
 lookup :: RainbowTable -> B.ByteString -> Maybe String
 lookup rt hashed = case HashMap.lookup hashed rt.table of
-  Just initialPw -> Just $ snd $ rt.genChain initialPw
-  a -> a
+  Just initialPw -> Just $ snd . last $ rt.genChain initialPw
+  a ->
+    let l = replicate rt.chainLength reduceHash
+        reduceHash = undefined
+     in undefined
